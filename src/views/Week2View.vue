93d0b714fd9a015b3635af5c101416e60c968d90
inventor9871 <118165822+inventor9871@users.noreply.github.com>
2025-08-12 14:14:33 +0800

    input_token

--- src/views/Week2View.vue
+++ src/views/Week2View.vue
@@ -1,119 +1,169 @@
 <template>
   <div class="container">
-    <div class="left">
-      <div class="vv">
-    <h1>註冊帳號</h1>
-    <!-- EMAIL： -->
-    <input type="email" v-model="sginupField.email" class="form-control" placeholder="請輸入EMAIL"/> <br />
-    <!-- 暱稱： -->
-    <input type="text" v-model="sginupField.nickname" class="form-control" placeholder="請輸入暱稱"/> <br />
-    <!-- 密碼： -->
-    <input type="password" v-model="sginupField.password" class="form-control" placeholder="請輸入密碼" /> <br />
-    <!-- 再次輸入密碼： -->
-    <input type="password" v-model="password2" class="form-control" placeholder="請再次輸入密碼" /> <br />
-    <span class="warn" v-if="pass12">{{ passwordIsMatch }} <br /></span>
-    <button type="button" @click="signup"   class="btn btn-info" >註冊帳號</button>
-  </div>
-  <hr />
+    <div class="row">
+      <div class="col">
+        <!-- 註冊帳號 -->
+        <div class="vv">
+          <h1>註冊帳號</h1>
+          <!-- EMAIL： -->
+          <input
+            type="email"
+            v-model="sginupField.email"
+            class="form-control"
+            placeholder="請輸入EMAIL"
+          />
+          <br />
+          <!-- 暱稱： -->
+          <input
+            type="text"
+            v-model="sginupField.nickname"
+            class="form-control"
+            placeholder="請輸入暱稱"
+          />
+          <br />
+          <!-- 密碼： -->
+          <input
+            type="password"
+            v-model="sginupField.password"
+            class="form-control"
+            placeholder="請輸入密碼"
+          />
+          <br />
+          <!-- 再次輸入密碼： -->
+          <input
+            type="password"
+            v-model="password2"
+            class="form-control"
+            placeholder="請再次輸入密碼"
+          />
+          <br />
+          <span class="warn" v-if="pass12">{{ passwordIsMatch }} <br /></span>
+          <button type="button" @click="signup" class="btn btn-info">註冊帳號</button>
+        </div>
+        <hr />
 
-  <div class="vv">
-    <h1>登入</h1>
-    <!-- EMAIL：  -->
-    <input type="email" v-model="signInField.email" class="form-control" placeholder="請輸入EMAIL" /> <br />
-    <!-- 密碼：  -->
-    <input type="password" v-model="signInField.password" class="form-control" placeholder="請輸入密碼" /> <br />
-    <button type="button" @click="signIn"  class="btn btn-info">登入</button> <br />
-    <p v-if="signInres.status == 200" class="success">
-      {{ signInres.nickname }} 您好，token = <br />
-      {{ signInres.token }}
-    </p>
-    <p v-else-if="signInres.status == 400" class="warn">帳號密碼錯誤</p>
-    <p v-else class="warn"></p>
-  </div>
-  <hr />
+        <!-- 登入 -->
+        <div class="vv">
+          <h1>登入</h1>
+          <!-- EMAIL：  -->
+          <input
+            type="email"
+            v-model="signInField.email"
+            class="form-control"
+            placeholder="請輸入EMAIL"
+          />
+          <br />
+          <!-- 密碼：  -->
+          <input
+            type="password"
+            v-model="signInField.password"
+            class="form-control"
+            placeholder="請輸入密碼"
+          />
+          <br />
+          <button type="button" @click="signIn" class="btn btn-info">登入</button> <br />
+          <p v-if="signInres.status == 200" class="success">
+            {{ signInres.nickname }} 您好，token = <br />
+            {{ signInres.token }}
+          </p>
+          <p v-else-if="signInres.status == 400" class="warn">帳號密碼錯誤</p>
+          <p v-else class="warn"></p>
+        </div>
+        <hr />
 
-
-  <div class="vv">
-    <h1>驗證</h1>
-    <!-- token： -->
-    <input type="text" v-model="inputToken" class="form-control" placeholder="請輸入token" /> <br />
-    <button type="button" @click="checkToken"  class="btn btn-info">驗證</button> <br />
-    <p class="success" v-if="checkOk">
-      驗證成功，您的UID: <br />
-      {{ uid }}
-    </p>
-  </div>
-  <hr />
-    </div>
-    <div class="right">
-
-      <div class="vv">
-        <h1>todoList</h1>
-        <input type="text" v-model="newTodo.content" class="form-control" placeholder="請輸入todo" />
-        <button type="button" @click="addTodo"  class="btn btn-info">新增todolist</button>　　
-        <button type="button" @click="refreshTodo" class="btn btn-success">重新取得Todo</button>
-        <div class="todo" v-for="(item, index) in showTodores" :key="index">
-          {{ new Date(item.createTime * 1000).toLocaleString('zh-TW') }} <br>
-          {{ item.content }} <br>
-          <input type="text" class="form-control" v-model="tempTodo.content" v-if="showi == index">
-          <br>
-          <button class="btn btn-warning" type="button" @click="editTodo(index)" v-if="editBut" >修改</button>　
-          <div v-if="showi == index">
-            <button type="button" @click="confirmTodo(index)">確認</button>　
-            <button type="button" @click="canTodo"> 取消</button>
+        <!-- 驗證 -->
+        <div class="vv">
+          <h1>驗證</h1>
+          <!-- token： -->
+          <input type="text" v-model="inputToken" class="form-control" placeholder="請輸入token" />
+          <br />
+          <button type="button" @click="checkToken" class="btn btn-info">驗證</button> <br />
+          <p class="success" v-if="checkOk">
+            驗證成功，您的UID: <br />
+            {{ uid }}
+          </p>
+        </div>
+        <hr />
+      </div>
+      <div class="col">
+        <!-- todoList -->
+        <div class="vv">
+          <h1>todoList</h1>
+          <input
+            type="text"
+            v-model="newTodo.content"
+            class="form-control"
+            placeholder="請輸入todo"
+          />
+          <button type="button" @click="addTodo" class="btn btn-info">新增todolist</button>　　
+          <button type="button" @click="refreshTodo" class="btn btn-success">重新取得Todo</button>
+          <div class="todo" v-for="(item, index) in showTodores" :key="index">
+            {{ new Date(item.createTime * 1000).toLocaleString('zh-TW') }} <br />
+            {{ item.content }} <br />
+            <input
+              type="text"
+              class="form-control"
+              v-model="tempTodo.content"
+              v-if="showi == index"
+            />
+            <br />
+            <button class="btn btn-warning" type="button" @click="editTodo(index)" v-if="editBut">
+              修改</button
+            >　
+            <div v-if="showi == index">
+              <button type="button" @click="confirmTodo(index)">確認</button>　
+              <button type="button" @click="canTodo">取消</button>
+            </div>
+            <button class="btn btn-danger" type="button" @click="delTodo(item.id, index)" v-else>
+              刪除
+            </button>
           </div>
-          <button class="btn btn-danger" type="button" @click="delTodo(item.id, index)" v-else>刪除</button>
-
         </div>
       </div>
     </div>
   </div>
-
-
 </template>
 <script setup>
 import { ref } from 'vue'
 import axios from 'axios'
 const url = 'https://todolist-api.hexschool.io'
 
-
-
-const refreshTodo = async()=>{
+const refreshTodo = async () => {
   await showTodo()
 }
-const delTodo = async (id, index)=>{
-  console.log('id=',id, ' todo=', showTodores.value[index].id)
+const delTodo = async (id, index) => {
+  console.log('id=', id, ' todo=', showTodores.value[index].id)
   const res = await axios.delete(`${url}/todos/${id}`, {
-    headers:{ Authorization: inputToken.value},
+    headers: { Authorization: inputToken.value },
   })
   console.log('delTodo', res.data)
   await showTodo()
 }
 
-const confirmTodo = (index)=>{
-  showTodores.value[index].content = tempTodo.value.content;
-  showi.value = '取消';
-  editBut.value = true;
+const confirmTodo = (index) => {
+  showTodores.value[index].content = tempTodo.value.content
+  showi.value = '取消'
+  editBut.value = true
   // tempTodo.value = {};
 }
 
-const canTodo = ()=>{
-  tempTodo.value = {};
-  showi.value = '取消';
-  editBut.value = true;
+const canTodo = () => {
+  tempTodo.value = {}
+  showi.value = '取消'
+  editBut.value = true
 }
 
-const tempTodo = ref({});
-const showi = ref('取消');
-const editBut = ref(true);
+const tempTodo = ref({})
+const showi = ref('取消')
+const editBut = ref(true)
 
-const editTodo = (index)=>{
-  if (showi.value ==='取消'){
-    showi.value = index;
-    editBut.value = false;
-    tempTodo.value = {...showTodores.value[index]};
-  }else{
-    showi.value ='取消'
+const editTodo = (index) => {
+  if (showi.value === '取消') {
+    showi.value = index
+    editBut.value = false
+    tempTodo.value = { ...showTodores.value[index] }
+  } else {
+    showi.value = '取消'
   }
 }
 
@@ -123,11 +173,15 @@ const newTodo = ref({
 const addTodo = async () => {
   try {
     console.log('newTodo.value', newTodo.value)
-    const res = await axios.post(`${url}/todos/`, {content: newTodo.value.content},{
-      headers:{
-        Authorization: inputToken.value,
-      }
-    })
+    const res = await axios.post(
+      `${url}/todos/`,
+      { content: newTodo.value.content },
+      {
+        headers: {
+          Authorization: inputToken.value,
+        },
+      },
+    )
     console.log('newTodo', res)
     showTodo()
   } catch (error) {
@@ -144,7 +198,7 @@ const showTodo = async () => {
     },
   })
   console.log('showtodo', res)
-  showTodores.value = {...res.data.data}
+  showTodores.value = { ...res.data.data }
   console.log('showTodores', showTodores.value)
 }
 
@@ -153,7 +207,9 @@ const uid = ref('')
 const checkOk = ref(false)
 const checkToken = async () => {
   const todoCookie = document.cookie.replace(
-    /(?:(?:^|.*;\s*)todoName\s*\=\s*([^;]*).*$)|^.*$/, '$1',  )
+    /(?:(?:^|.*;\s*)todoName\s*\=\s*([^;]*).*$)|^.*$/,
+    '$1',
+  )
 
   const res = await axios.get(`${url}/users/checkout`, {
     headers: {
@@ -161,13 +217,13 @@ const checkToken = async () => {
     },
   })
 
-  console.log('checkToken ',res);
+  console.log('checkToken ', res)
+
   if (inputToken.value === todoCookie) {
     uid.value = res.data.uid
     checkOk.value = true
     showTodo()
     console.log(todoCookie)
-
   } else {
     checkOk.value = false
   }
@@ -187,12 +243,13 @@ const signIn = async () => {
   try {
     // console.log(`${url}/users/sign_in`)
     const res = await axios.post(`${url}/users/sign_in`, signInField.value)
-    console.log(res);
-    signInres.value.nickname = res.data.nickname;
-    signInres.value.token = res.data.token;
-    signInres.value.status = res.status;
-    const expireDate = new Date(res.data.exp * 1000);
-     expireDate.setDate(expireDate.getDate() -1);
+    console.log(res)
+    signInres.value.nickname = res.data.nickname
+    signInres.value.token = res.data.token
+    signInres.value.status = res.status
+    inputToken.value = res.data.token
+    const expireDate = new Date(res.data.exp * 1000)
+    expireDate.setDate(expireDate.getDate() - 1)
     document.cookie = `todoName=${res.data.token}; expires=${expireDate.toUTCString()}; path=/`
     console.log('expireDate= ', expireDate)
   } catch (error) {
@@ -245,7 +302,7 @@ const signup = async () => {
   padding: 12px;
   margin: 12px;
 }
-.todo{
+.todo {
   border: 1px solid gray;
   border-radius: 20px;
   padding: 12px;
@@ -255,8 +312,9 @@ const signup = async () => {
 /* .container{
   display: flex;
 max-width: 987px;;
-}
-.left, .right{
-  max-width: 500px;
+} */
+
+/* .left, .right{
+  max-width: 45%;
 } */
 </style>
